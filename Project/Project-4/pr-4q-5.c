86eb54161fc078ac9e01db497a8c3a38069c5952
Om Kadam <kadamom0017@gmail.com>
2026-02-06 10:17:52 +0530

    project added

---
+++ Project/Project-4/pr-4q-5.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+   int n = 5;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int s = 1; s <= n-i; s++)
+            printf(" ");
+        for (int j = n; j )
---
+++ Project/Project-5/pr-5_q1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    
+    int a[5] = {2,-4,1,-3,-5};
+
+    printf("%d ",a[1]);
+    printf("%d ",a[3]);
+    printf("%d ",a[4]);
+
+    return 0;
+}
